b2f1f8447a4b2d93b0ea93d254334a4da959ec0e
intan1907 <intan3951@gmail.com>
2019-02-23 17:28:33 +0700

    show emergency call number on summary

--- src/app/src/main/java/com/pbd/housecure/housecure/SettingsFragment.java
+++ src/app/src/main/java/com/pbd/housecure/housecure/SettingsFragment.java
@@ -1,18 +1,15 @@
 package com.pbd.housecure.housecure;
 
 
-import android.os.Build;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v7.preference.PreferenceManager;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-
+    private SharedPreferences sharedPreferences;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -20,7 +17,27 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.preferences, rootKey);
+        addPreferencesFromResource(R.xml.preferences);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        setEmergencySummary();
     }
 
+    public void setEmergencySummary() {
+        final String emergencyKey = getString(R.string.pref_emergency_key);
+        String emergencyDefault = getString(R.string.pref_emergency_default_value);
+        String emergencyValue = sharedPreferences.getString(emergencyKey, emergencyDefault);
+
+        final EditTextPreference editTextPreference = (EditTextPreference) findPreference(emergencyKey);
+        editTextPreference.setSummary(emergencyValue);
+
+        editTextPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                String newValue = o.toString();
+                sharedPreferences.edit().putString(emergencyKey, newValue).apply();
+                editTextPreference.setSummary(newValue);
+                return true;
+            }
+        });
+    }
 }
