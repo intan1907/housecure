90bdefebc179d11225bd0441dd710c371857eb7e
Kelvin Kristian <13516101@std.stei.itb.ac.id>
2019-02-24 22:54:18 +0700

    fixed VOLLEY on AddDeviceFragment

--- src/app/src/main/java/com/pbd/housecure/housecure/AddDeviceFragment.java
+++ src/app/src/main/java/com/pbd/housecure/housecure/AddDeviceFragment.java
@@ -12,65 +12,32 @@ import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddDeviceFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddDeviceFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class AddDeviceFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
     public AddDeviceFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddDeviceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddDeviceFragment newInstance(String param1, String param2) {
-        AddDeviceFragment fragment = new AddDeviceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -94,8 +61,7 @@ public class AddDeviceFragment extends Fragment {
                     obj.put("device_id", device_id);
                 } catch (Exception e) {
                     Log.e("VOLLEY", e.toString());
-                }
-
+                };
                 JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, obj,
                         new Response.Listener<JSONObject>() {
                             @Override
@@ -108,20 +74,21 @@ public class AddDeviceFragment extends Fragment {
                         Log.e("VOLLEY", error.toString());
                     }
                 }
-                );
-
+                ) {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("Content-Type", "application/json; charset=UTF-8");
+                        params.put("Authorization", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                        return params;
+                    }
+                };
                 queue.add(jsonRequest);
             }
         });
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
@@ -131,21 +98,5 @@ public class AddDeviceFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
