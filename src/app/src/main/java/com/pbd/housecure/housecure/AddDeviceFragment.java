0429af0d126d071a672b1936c45589b570138c1c
Kelvin Kristian <13516101@std.stei.itb.ac.id>
2019-02-25 18:14:12 +0700

    merging

--- src/app/src/main/java/com/pbd/housecure/housecure/AddDeviceFragment.java
+++ src/app/src/main/java/com/pbd/housecure/housecure/AddDeviceFragment.java
@@ -1,7 +1,6 @@
 package com.pbd.housecure.housecure;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
