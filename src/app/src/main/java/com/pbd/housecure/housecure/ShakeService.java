1bce6cf9a7e9c50a8bbbfeab6808a31b02470e5f
Kelvin Kristian <13516101@std.stei.itb.ac.id>
2019-02-25 14:22:09 +0700

    calibrated accelerometer value at ShakeService

--- src/app/src/main/java/com/pbd/housecure/housecure/ShakeService.java
+++ src/app/src/main/java/com/pbd/housecure/housecure/ShakeService.java
@@ -13,7 +13,22 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
+import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class ShakeService extends Service implements SensorEventListener {
@@ -50,13 +65,43 @@ public class ShakeService extends Service implements SensorEventListener {
         mAccel = mAccel * 0.9f + delta;
 
 
-        if (mAccel > 13) {
+        if (mAccel > 40) {
             vibrator.vibrate(200);
+            turnLightsOn();
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    public void turnLightsOn() {
+        RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+        String url = getResources().getString(R.string.api_host) + "/lights/on";
+
+        JSONObject obj = new JSONObject();
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, obj,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("VOLLEY", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", error.toString());
+            }
+        }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json; charset=UTF-8");
+                params.put("Authorization", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                return params;
+            }
+        };
+        queue.add(jsonRequest);
     }
 }
